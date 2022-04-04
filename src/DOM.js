0a8bd46cfd0e043828374aae83a3a7ae4f29696e
MariBayd <masha.baydina@gmail.com>
2022-04-04 12:19:31 +0300

    Задание на работу с DOM

--- src/DOM.js
+++ src/DOM.js
@@ -5,6 +5,11 @@
   Считаем, что всегда передается тег, допускающий вставку текста в качестве своего содержимого (P, DIV, I и пр.).
 */
 export function appendToBody(tag, content, count) {
+    for (let i = 0; i < count; i++) {
+        let elem = document.createElement(tag);
+        elem.textContent = content;
+        document.body.insertAdjacentElement('beforeend', elem);
+    }
 }
 
 /*
@@ -14,7 +19,27 @@ export function appendToBody(tag, content, count) {
   Каждый элемент должен иметь класс вида item_n, где n - глубина вложенности элемента. (Нумерацию ведем с единицы).
   Сформированное дерево верните в качестве результата работы функции.
 */
+function recursion(childrenCount, level, treeResult, maxLevel) {
+    if (level <= 0) return;
+
+    for (let i = 0; i < childrenCount; i++) {
+        let treeChild = document.createElement('div');
+        treeChild.setAttribute('class', 'item_' + (maxLevel - level + 1));
+        treeResult.insertAdjacentElement('afterbegin', treeChild);
+
+        recursion(childrenCount, level - 1, treeChild, maxLevel);
+    }
+}
+
 export function generateTree(childrenCount, level) {
+    if (level <= 0) return null;
+
+    let root = document.createElement('div');
+    root.setAttribute('class', 'item_1');
+
+    recursion(childrenCount, level - 1, root, level);
+
+    return root;
 }
 
 /*
@@ -25,5 +50,21 @@ export function generateTree(childrenCount, level) {
   которые находились внутри переписанных тегов.
   Сформированное дерево верните в качестве результата работы функции.
 */
+
 export function replaceNodes() {
+    let tree = generateTree(2, 3);
+
+    for (let i = 0; i < tree.childNodes.length; i++) {
+        let newSection = document.createElement('section');
+        newSection.classList.add('item_2');
+
+        if (tree.childNodes[i].classList.contains('item_2')) {
+            let children = tree.childNodes[i].innerHTML;
+
+            newSection.insertAdjacentHTML('afterbegin', children);
+            tree.childNodes[i].replaceWith(newSection);
+        }
+    }
+
+    return tree;
 }
